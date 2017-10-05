9affd5cac5f5067e3588e60f98b7fcb5eb43d720
Guillaume Hivert <hivert.is.coming@gmail.com>
2017-10-05 09:06:01 +0200

    Format demo html

--- demo/00-Html.elm
+++ demo/00-Html.elm
@@ -9,40 +9,42 @@ import BoxShadow
 import Border
 import Color exposing (Color)
 
+
 main : Html msg
 main =
-  Html.div
-    [ Html.Attributes.style
-      <| Elegant.toInlineStyles
-      <| flip Elegant.style []
-      <| Just
-      <| Elegant.displayBlock []
-        [ Elegant.typography
-          [ Elegant.color Color.blue
-          , Typography.character
-            [ Typography.Character.weight 900
-            , Typography.Character.size (px 200)
-            , Typography.Character.italic
-            ]
-          ]
-        , Elegant.padding (px 30)
-        , Elegant.boxShadow
-          [ BoxShadow.standard (px 30) Color.black (px 3, px 3)
-          , BoxShadow.inset True
-          ]
-        , Elegant.border
-          [ Border.top
-            [ Elegant.color Color.blue
-            , Border.solid
-            ]
-          , Border.horizontal
-            [ Elegant.color Color.black ]
-          ]
-        -- , Elegant.margin
-        --   [ Margin.width (px 30)
-        --   , Margin.bottom Margin.default
-        --     [ Margin.Side.width (px 200) ]
-        --   ]
+    Html.div
+        [ Html.Attributes.style <|
+            Elegant.toInlineStyles <|
+                flip Elegant.style [] <|
+                    Just <|
+                        Elegant.displayBlock []
+                            [ Elegant.typography
+                                [ Elegant.color Color.blue
+                                , Typography.character
+                                    [ Typography.Character.weight 900
+                                    , Typography.Character.size (px 200)
+                                    , Typography.Character.italic
+                                    ]
+                                ]
+                            , Elegant.padding (px 30)
+                            , Elegant.boxShadow
+                                [ BoxShadow.standard (px 30) Color.black ( px 3, px 3 )
+                                , BoxShadow.inset True
+                                ]
+                            , Elegant.border
+                                [ Border.top
+                                    [ Elegant.color Color.blue
+                                    , Border.solid
+                                    ]
+                                , Border.horizontal
+                                    [ Elegant.color Color.black ]
+                                ]
+
+                            -- , Elegant.margin
+                            --   [ Margin.width (px 30)
+                            --   , Margin.bottom Margin.default
+                            --     [ Margin.Side.width (px 200) ]
+                            --   ]
+                            ]
         ]
-    ]
-    [ Html.text "Just a text." ]
+        [ Html.text "Just a text." ]
