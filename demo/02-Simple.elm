4b5ad5f13fda12ba263014d0829754f907db00fb
Guillaume Hivert <hivert.is.coming@gmail.com>
2017-10-18 14:19:22 +0200

    Refactor 02-Simple.elm

--- demo/02-Simple.elm
+++ demo/02-Simple.elm
@@ -1,7 +1,12 @@
 module Main exposing (..)
 
-import BodyBuilder exposing (..)
-import Elegant exposing (textCenter, padding, SizeUnit(..), fontSize)
+import BodyBuilder exposing (Node)
+import BodyBuilder.Attributes
+import Elegant exposing (px, Style)
+import Display
+import Layout
+import Typography
+import Typography.Character as Character
 
 
 theFontSize : number
@@ -11,19 +16,42 @@ theFontSize =
 
 view : a -> Node msg
 view model =
-    div [ style [ textCenter ] ]
-        [ h1 [] [ text "I'm Elegantly styled by css" ]
-        , p
-            [ style [ fontSize (Px theFontSize) ]
-            , hoverStyle [ fontSize (Px (theFontSize + 5)) ]
+    BodyBuilder.div
+        [ BodyBuilder.Attributes.style
+            [ Display.block
+                [ Display.alignCenter ]
+                []
+                |> Elegant.style
             ]
-            [ text "Go Hover Me ! My style is set in my node, so it's parametrizable, but the result is a good old css node (look at the dom Luke)" ]
+        ]
+        [ BodyBuilder.h1 []
+            [ BodyBuilder.text "I'm Elegantly styled by css" ]
+        , BodyBuilder.p
+            [ BodyBuilder.Attributes.style
+                [ Display.block []
+                    [ Layout.typography
+                        [ Typography.character
+                            [ Character.size (px theFontSize) ]
+                        ]
+                    ]
+                    |> Elegant.style
+                , Display.block []
+                    [ Layout.typography
+                        [ Typography.character
+                            [ Character.size (px (theFontSize + 5)) ]
+                        ]
+                    ]
+                    |> Elegant.style
+                    |> Elegant.setSuffix "hover"
+                ]
+            ]
+            [ BodyBuilder.text "Go Hover Me ! My style is set in my node, so it's parametrizable, but the result is a good old css node (look at the dom Luke)" ]
         ]
 
 
 main : Program Basics.Never Int msg
 main =
-    program
+    BodyBuilder.program
         { init = ( 0, Cmd.none )
         , update = (\msg model -> ( model, Cmd.none ))
         , subscriptions = always Sub.none
