75bef5d7e84faa899d4b1f1ec680e18f3e9e3fa0
Thibaut Assus <thibaut@milesrock.com>
2017-11-07 14:16:04 +0100

    Improve graphics

--- wysiwyg/Main.elm
+++ wysiwyg/Main.elm
@@ -4,6 +4,7 @@ import BodyBuilder as B exposing (Node)
 import BodyBuilder.Attributes as A
 import BodyBuilder.Events as E
 import Style as S
+import Constants as C
 import Color exposing (Color)
 import Elegant exposing (px, vh, percent, Modifiers, Modifier)
 import Box
@@ -706,7 +707,10 @@ creationView model =
                     (\( msg, str ) ->
                         B.flexItem []
                             [ B.button
-                                [ A.style [ S.block [ Block.height (percent 100) ] ]
+                                [ A.style
+                                    [ S.block [ Block.height (percent 100), Block.width (px 50) ]
+                                    , S.box [ Box.borderNone, Box.backgroundColor (Color.rgba 0 0 0 0) ]
+                                    ]
                                 , E.onClick msg
                                 ]
                                 [ B.text str ]
@@ -1025,7 +1029,7 @@ inspectorView model =
                 B.div [] [ B.text "Nothing" ]
 
             Just { id, tree } ->
-                B.div []
+                B.div [ A.style [ S.box [ Box.paddingAll (C.medium) ] ] ]
                     [ B.h1 [] [ B.text "Inspector" ]
                     , case tree of
                         Text t ->
@@ -1060,7 +1064,7 @@ inspectorView model =
 
 treeView : Model -> Node Msg
 treeView { selectedId, element } =
-    B.div []
+    B.div [ A.style [ S.box [ Box.paddingAll (C.medium) ] ] ]
         [ B.h1 [] [ B.text "Tree view" ]
         , displayTreeView selectedId element
         ]
